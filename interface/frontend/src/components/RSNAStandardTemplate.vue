a32bc8c250e399d5eda836b0d72115417d3725ed
Alan Neigh <alanneigh@bugmenot.ml>
2017-10-26 01:45:52 -0400

    Separate RSNA template into a component from report and export Prepare to migrate styles #182

---
+++ interface/frontend/src/components/RSNAStandardTemplate.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="rsna-standard-template-container">
+  <header>
+    <h1>RSNA Standard Template</h1>
+  </header>
+
+	<section id="technical-parameters">
+		<h2>Technical parameters</h2>
+
+		<li>kVp: {{ rsna.kVp }}</li>
+		<li>mA: {{ rsna.mA }}</li>
+		<li>DLP: {{ rsna.DLP }}</li>
+	</section>
+
+	<section id="clinical-information">
+		<h2>Clinical information</h2>
+		<li>Screening visit: {{ rsna.screeningVisit }}</li>
+		<li>{{ rsna.clinicalInformation }}</li>
+	</section>
+
+	<section id="findings">
+		<h2>Findings</h2>
+
+		<h3>Exam parameters</h3>
+		<li>Diagnostic quality: {{ rsna.diagnosticQuality }}</li>
+		<li>Comments: {{ rsna.examParametersComment }}</li>
+
+		<h3>Lung nodules</h3>
+
+		<li v-for='nodule in nodules'>
+			<a href='#' @click='selectSeries(series)'>{{ series.series_instance_uid }}</a>
+			<span v-if='series == selected'>&larr;</span>
+		</li>
+
+		<h4>Lungs</h4>
+		<li>COPD: {{ rsna.COPD }}</li>
+		<li>Filiosis: {{ rsna.filiosis }}</li>
+		<li>Lymph nodes: {{ rsna.lymphNodes }}</li>
+		<li>Other findings: {{ rsna.otherFindings }}</li>
+
+		<h4>Right pleural space</h4>
+		<li>Effusion: {{ rsna.rightPleuralSpace.effusion }}</li>
+		<li>Calcification: {{ rsna.rightPleuralSpace.calcification }}</li>
+		<li>Thickening: {{ rsna.rightPleuralSpace.thickening }}</li>
+		<li>Pneumothorax: {{ rsna.rightPleuralSpace.pneumothorax }}</li>
+
+		<h4>Left pleural space</h4>
+		<li>Effusion: {{ rsna.rightPleuralSpace.effusion }}</li>
+		<li>Calcification: {{ rsna.rightPleuralSpace.calcification }}</li>
+		<li>Thickening: {{ rsna.rightPleuralSpace.thickening }}</li>
+		<li>Pneumothorax: {{ rsna.rightPleuralSpace.pneumothorax }}</li>
+
+		<h4>Heart</h4>
+		<li>Heart size: {{ rsna.heartSize }}</li>
+		<li>Coronary calcification: {{ rsna.coronaryCalcification }}</li>
+		<li>Pericardial effusion: {{ rsna.pericardialEffusion }}</li>
+
+		<h4>Other</h4>
+		<li>Upper abdomen: {{ rsna.upper_abdomen }}</li>
+		<li>Thorax: {{ rsna.thorax }}</li>
+		<li>Base of neck: {{ rsna.base_of_neck }}</li>
+		<h2>Impression</h2>
+		<li>Need comparison: {{ rsna.need_comparison }}</li>
+		<li>Repeat CT: {{ rsna.repeatCT }}</li>
+		<li>See physician: {{ rsna.see_physician }}</li>
+		<li>{{ rsna.impressionComment }}</li>
+	</section>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['rsna', 'nodules'],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.rsna-standard-template-container {
+    }
+</style>
--- interface/frontend/src/views/ReportAndExport.vue
+++ interface/frontend/src/views/ReportAndExport.vue
@@ -1,116 +1,79 @@
 <template>
-  <div>
-    <h2>Technical parameters</h2>
-    <li>kVp: {{ study.kVp }}</li>
-    <li>mA: {{ study.mA }}</li>
-    <li>DLP: {{ study.DLP }}</li>
-
-    <h2>Clinical information</h2>
-    <li>Screening visit: {{ study.screeningVisit }}</li>
-    <li>{{ study.clinicalInformation }}</li>
-
-
-    <h2>Comparison</h2>
-    <li>{{ study.comparison }}</li>
-
-    <h2>Findings</h2>
-    <h4>Exam parameters</h4>
-    <li>Diagnostic quality: {{ study.diagnosticQuality }}</li>
-    <li>Comments: {{ study.examParametersComment }}</li>
-
-    <h4>Lung nodules</h4>
-
-    {% for nodule in study.nodules %}
-    <li>{{ nodule.diameter }} </li>
-    <li>{{ nodule.appearanceFeature }} </li>
-    <li>{{ nodule.densityFeature }} </li>
-    <li>{{ nodule.imageNo }} </li>
-    {% endfor %}
-
-    <li v-for='nodule in study.nodules'>
-      <a href='#' @click='selectSeries(series)'>{{ series.series_instance_uid }}</a>
-      <span v-if='series == selected'>&larr;</span>
-    </li>
-
-
-    <h4>Lungs</h4>
-    <li>COPD: {{ study.COPD }}</li>
-    <li>Filiosis: {{ study.filiosis }}</li>
-    <li>Lymph nodes: {{ study.lymphNodes }}</li>
-    <li>Other findings: {{ study.otherFindings }}</li>
-
-    <h4>Right pleural space</h4>
-    <li>Effusion: {{ study.rightPleuralSpace.effusion }}</li>
-    <li>Calcification: {{ study.rightPleuralSpace.calcification }}</li>
-    <li>Thickening: {{ study.rightPleuralSpace.thickening }}</li>
-    <li>Pneumothorax: {{ study.rightPleuralSpace.pneumothorax }}</li>
-
-    <h4>Left pleural space</h4>
-    <li>Effusion: {{ study.rightPleuralSpace.effusion }}</li>
-    <li>Calcification: {{ study.rightPleuralSpace.calcification }}</li>
-    <li>Thickening: {{ study.rightPleuralSpace.thickening }}</li>
-    <li>Pneumothorax: {{ study.rightPleuralSpace.pneumothorax }}</li>
-
-    <h4>Heart</h4>
-    <li>Heart size: {{ study.heartSize }}</li>
-    <li>Coronary calcification: {{ study.coronaryCalcification }}</li>
-    <li>Pericardial effusion: {{ study.pericardialEffusion }}</li>
-
-    <h4>Other findings</h4>
-    <li>Upper abdomen: {{ study.upper_abdomen }}</li>
-    <li>Thorax: {{ study.thorax }}</li>
-    <li>Base of neck: {{ study.base_of_neck }}</li>
-    <h2>Impression</h2>
-    <li>Need comparison: {{ study.need_comparison }}</li>
-    <li>Repeat CT: {{ study.repeatCT }}</li>
-    <li>See physician: {{ study.see_physician }}</li>
-    <li>{{ study.impressionComment }}</li>
-
+<div class="offset-top">
+  <div class="outline-nav">
+		<h1>Outline</h1>
+    <ul>
+      <li v-for='outline in outlines'>
+        <a href='#' @click='jumpToOutline(outline)'>{{ outline }}</a>
+      </li>
+    </ul>
   </div>
+
+  <rsna-standard-template :rsna="study" :nodules="nodules"></rsna-standard-template>
+
+</div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        study: {
-          kVp: null,
-          mA: null,
-          DLP: null,
-          screeningVisit: 'BL',
-          clinicalInformation: '',
-          comparison: 'null',
-          diagnosticQuality: 'SF',
-          examParametersComment: '',
-          COPD: 0,
-          filiosis: 0,
-          lymphNodes: 'null.',
-          otherFindings: 'null.',
-          rightPleuralSpace: {
-            effusion: 0,
-            calcification: 0,
-            thickening: 0,
-            pneumothorax: 0
-          },
-          leftPleuralSpace: {
-            effusion: 0,
-            calcification: 0,
-            thickening: 0,
-            pneumothorax: 0
-          },
-          heartSize: 0,
-          coronaryCalcification: 0,
-          pericardialEffusion: 0,
-          upperAbdomen: 'null',
-          thorax: 'null',
-          baseOfNeck: 'null',
-          needComparison: 'No',
-          repeatCT: 'T1',
-          seePhysician: 'NO',
-          impressionComment: ''
+import RSNAStandardTemplate from '../components/RSNAStandardTemplate'
+
+export default {
+  components: {
+    'rsna-standard-template': RSNAStandardTemplate
+  },
+  data () {
+    return {
+      outlines: ['Overview', 'RSNAStandardTemplate', 'ACR Lung-RAD tm Findings', 'Export 3D Imagery'],
+      study: {
+        kVp: null,
+        mA: null,
+        DLP: null,
+        screeningVisit: 'BL',
+        clinicalInformation: '',
+        comparison: 'null',
+        diagnosticQuality: 'SF',
+        examParametersComment: '',
+        COPD: 0,
+        filiosis: 0,
+        lymphNodes: 'null.',
+        otherFindings: 'null.',
+        rightPleuralSpace: {
+          effusion: 0,
+          calcification: 0,
+          thickening: 0,
+          pneumothorax: 0
         },
-        nodule: []
-      }
+        leftPleuralSpace: {
+          effusion: 0,
+          calcification: 0,
+          thickening: 0,
+          pneumothorax: 0
+        },
+        heartSize: 0,
+        coronaryCalcification: 0,
+        pericardialEffusion: 0,
+        upperAbdomen: 'null',
+        thorax: 'null',
+        baseOfNeck: 'null',
+        needComparison: 'No',
+        repeatCT: 'T1',
+        seePhysician: 'NO',
+        impressionComment: ''
+      },
+      nodules: []
     }
   }
+}
 </script>
+
+<style media="screen">
+.offset-top {
+  margin-top: 5em;
+}
+
+@media screen and (max-width: 990px) {
+  .offset-top {
+    margin-top: 10em;
+  }
+}
+</style>
